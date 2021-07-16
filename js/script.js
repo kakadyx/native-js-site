5572884f47294840090a55f2fa004580475f017c
kakadyx <mintesok@yandex.ru>
2021-07-16 14:08:11 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+
+import tabs from "./modules/tabs";
+import forms from "./modules/forms";
+import modal from "./modules/modal";
+import calc from "./modules/calc";
+import timer from "./modules/timer";
+import slider from "./modules/slider";
+import cards from "./modules/cards";
+import {openModal} from "./modules/modal";
+
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    const modalTimerId = setTimeout(()=>openModal('.modal',modalTimerId), 300000);
+
+    tabs('.tabheader__item','.tabcontent','.tabheader__items','tabheader__item_active');
+    modal('[data-modal]','.modal',modalTimerId);
+    timer('.timer','2020-06-11');
+    cards();
+    calc();
+    forms('form',modalTimerId);
+    slider({
+        container:'.offer__slider',
+        slide: '.offer__slide',
+        prevArrow: '.offer__slider-prev',
+        nextArrow: '.offer__slider-next',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper:  '.offer__slider-wrapper',
+        field: '.offer__slider-inner',
+    });
+  
+    
+
+});
